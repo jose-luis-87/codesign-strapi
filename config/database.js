6446130664fe2bde99d352e06ebe4354463392e4
José Luis Mandujano <mdi.proyecto@gmail.com>
2021-08-17 11:03:34 -0500

    env mongo

--- config/database.js
+++ config/database.js
@@ -8,8 +8,8 @@ module.exports = ({ env }) => ({
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME', 'codesignDB'),
-        username: env('DATABASE_USERNAME', 'Admin'),
-        password: env('DATABASE_PASSWORD', 'Codesign*2021'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
