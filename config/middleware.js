43428ca6da0df61f8f640037170758a4c70806c4
José Luis Mandujano <mdi.proyecto@gmail.com>
2021-08-18 16:27:48 -0500

    cors ettings

--- config/middleware.js
+++ config/middleware.js
@@ -1,20 +1,12 @@
 module.exports = {
+    load: {
+      before: ['timer', 'responseTime', 'logger', 'cors', 'responses', 'gzip'],
+      after: ['parser', 'router'],
+    },
     settings: {
       cors: {
         enabled: true,
-        origin: '*',
-        expose: ['WWW-Authenticate', 'Server-Authorization', 'Access-Control-Expose-Headers'],
-        maxAge: 31536000,
-        credentials: true,
-        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
-        headers: [
-          'Content-Type',
-          'Authorization',
-          'X-Frame-Options',
-          'Origin',
-          'Access-Control-Allow-Headers',
-          'access-control-allow-origin',
-        ],
+        origin: ['*']
       },
     },
-  };
+};
