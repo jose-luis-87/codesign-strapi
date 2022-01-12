a9ef0e550390cff452bf17081a2a7e0aafca32f3
José Luis Mandujano <57630829+jose-luis-87@users.noreply.github.com>
2022-01-12 11:42:06 -0600

    merge

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
@@ -20,6 +21,8 @@ module.exports = ({ env }) => ({
   },
 });
 =======
+=======
+>>>>>>> parent of 11d4409 (server)
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
@@ -30,8 +33,13 @@ module.exports = ({ env }) => ({
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME', 'codesignDB'),
+<<<<<<< HEAD
         username: env('DATABASE_USERNAME', 'Admin'),
         password: env('DATABASE_PASSWORD', '12345'),
+=======
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+>>>>>>> parent of 11d4409 (server)
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
@@ -40,4 +48,7 @@ module.exports = ({ env }) => ({
     },
   },
 });
+<<<<<<< HEAD
 >>>>>>> 5c4b4a1fdf05101e6b4895ce256d6472cc82f164
+=======
+>>>>>>> parent of 11d4409 (server)
